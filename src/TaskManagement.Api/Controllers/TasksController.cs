ea7556f9510111b1bd81a16bfd98cd815170a95c
Sandro Sanchez <sandropucp@gmail.com>
2024-09-05 22:54:24 -0400

    Verify update (#4)
    * break code
    
    * Update
    
    * workflow name update
    
    * test1

--- src/TaskManagement.Api/Controllers/TasksController.cs
+++ src/TaskManagement.Api/Controllers/TasksController.cs
@@ -31,51 +31,52 @@ public class TasksController : ApiController
                 statusCode: StatusCodes.Status400BadRequest,
                 detail: "Invalid task status");
         }
+    }
 
-        var command = new CreateTaskCommand(request.Name, request.Description,
-            request.DueDate, taskStatus, request.CategoryId, request.UserId);
+    var command = new CreateTaskCommand(request.Name, request.Description,
+        request.DueDate, taskStatus, request.CategoryId, request.UserId);
 
-        var createTaskResult = await _mediator.Send(command);
+    var createTaskResult = await _mediator.Send(command);
         return createTaskResult.MatchFirst(
             task => Ok(new TaskResponse(task.Id, task.Name)),
             error => Problem(error));
     }
 
-    [HttpGet("{taskId:guid}")]
-    public async Task<IActionResult> GetTask(Guid taskId)
-    {
-        var query = new GetTaskQuery(taskId);
+[HttpGet("{taskId:guid}")]
+public async Task<IActionResult> GetTask(Guid taskId)
+{
+    var query = new GetTaskQuery(taskId);
 
-        var getTaskResult = await _mediator.Send(query);
+    var getTaskResult = await _mediator.Send(query);
 
-        return getTaskResult.Match(
-            task => Ok(new TaskResponse(
-                task.Id,
-                task.Name)),
-            Problem);
-    }
+    return getTaskResult.Match(
+        task => Ok(new TaskResponse(
+            task.Id,
+            task.Name)),
+        Problem);
+}
 
-    [HttpDelete("{taskId:guid}")]
-    public async Task<IActionResult> DeleteTask(Guid taskId)
-    {
-        var command = new DeleteTaskCommand(taskId);
+[HttpDelete("{taskId:guid}")]
+public async Task<IActionResult> DeleteTask(Guid taskId)
+{
+    var command = new DeleteTaskCommand(taskId);
 
-        var deleteTaskResult = await _mediator.Send(command);
+    var deleteTaskResult = await _mediator.Send(command);
 
-        return deleteTaskResult.Match(
-            _ => NoContent(),
-            Problem);
-    }
+    return deleteTaskResult.Match(
+        _ => NoContent(),
+        Problem);
+}
 
-    [HttpGet]
-    public async Task<IActionResult> ListTasks()
-    {
-        var command = new ListTasksQuery();
+[HttpGet]
+public async Task<IActionResult> ListTasks()
+{
+    var command = new ListTasksQuery();
 
-        var listTasksResult = await _mediator.Send(command);
+    var listTasksResult = await _mediator.Send(command);
 
-        return listTasksResult.Match(
-            tasks => Ok(tasks.ConvertAll(task => new TaskResponse(task.Id, task.Name))),
-            Problem);
-    }
+    return listTasksResult.Match(
+        tasks => Ok(tasks.ConvertAll(task => new TaskResponse(task.Id, task.Name))),
+        Problem);
+}
 }
